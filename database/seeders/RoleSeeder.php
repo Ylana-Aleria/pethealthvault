93d9d212d0b9311c976a6f5f38605fe41c72fb49
priestess-foresa <foresaverde@gmail.com>
2025-05-24 07:46:07 +0800

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,22 +1,17 @@
-<?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
 use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
     public function run()
     {
-        Role::create([
-            'name' => 'Admin',
-            'description' => 'Administrator role',
-        ]);
+        Role::firstOrCreate(
+            ['name' => 'Admin'],
+            ['description' => 'Administrator role']
+        );
 
-        Role::create([
-            'name' => 'Owner',
-            'description' => 'Owner role',
-        ]);
+        Role::firstOrCreate(
+            ['name' => 'Owner'],
+            ['description' => 'Pet owner role']
+        );
     }
 }
