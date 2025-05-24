ac5e7e5082b24ec91a59681e92ce10853e04e65a
priestess-foresa <foresaverde@gmail.com>
2025-05-24 09:42:06 +0800

    Create VerifyCsrfToken.php

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    // You can add exceptions here if needed, e.g.
+    // protected $except = [
+    //     'api/*',
+    // ];
+}
