e82d10d00b56bc66b1ea8e91ec7e95d2b1d07269
priestess-foresa <foresaverde@gmail.com>
2025-05-24 12:07:26 +0800

    Update session.php

--- config/session.php
+++ config/session.php
@@ -18,7 +18,35 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'file'),
+
+    'lifetime' => 120,
+
+    'expire_on_close' => false,
+
+    'encrypt' => false,
+
+    'files' => storage_path('framework/sessions'),
+
+    'connection' => null,
+
+    'table' => 'sessions',
+
+    'store' => null,
+
+    'lottery' => [2, 100],
+
+    'cookie' => env('SESSION_COOKIE', Str::slug(env('APP_NAME', 'laravel'), '_').'_session'),
+
+    'path' => '/',
+
+    'domain' => env('SESSION_DOMAIN', null),
+
+    'secure' => env('SESSION_SECURE_COOKIE', true), // <- MUST be true for HTTPS
+
+    'http_only' => true,
+
+    'same_site' => 'lax', // try 'lax' first. If cross-domain, try 'none'
 
     /*
     |--------------------------------------------------------------------------
